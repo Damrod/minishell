c15765a9e81b2a5d26de7ccd9f5451bcd301b586
Alvaro Ollero <aollero@gmail.com>
2021-06-26 23:24:36 +0300

    draft of splitting for norminette get_redirs, not tested

--- srcs/input_output/get_redirs.c
+++ srcs/input_output/get_redirs.c
@@ -7,6 +7,85 @@
 #include <get_tokens.h>
 #include <heredoc.h>
 
+int	to_input(int *input, char *file, int *prunepattern, bool is_last)
+{
+	if (!is_last)
+	{
+		if (*input > STDIN_FILENO)
+			close(*input);
+		*input = open(file, O_RDONLY);
+		if (*input == -1)
+			return (error_file(file, prunepattern));
+		return (0);
+	}
+	else
+		return (error_syntax("newline", prunepattern));
+}
+
+int	to_append(int *output, char *file, int *prunepattern, bool is_last)
+{
+	if (!is_last)
+	{
+		if (*output > STDERR_FILENO)
+			close(*output);
+		*output = open(file, O_CREAT | O_WRONLY | O_APPEND, S_IRUSR
+				| S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+		if (*output == -1)
+			return (error_file(file, prunepattern));
+		return (0);
+	}
+	else
+		return (error_syntax("newline", prunepattern));
+}
+
+int	to_output(int *output, char *file, int *prunepattern, bool is_last)
+{
+	if (!is_last)
+	{
+		if (*output > STDERR_FILENO)
+			close(*output);
+		*output = open(file, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR
+				| S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+		if (*output == -1)
+			return (error_file(file, prunepattern));
+		return (0);
+	}
+	else
+		return (error_syntax("newline", prunepattern));
+}
+
+int	to_heredoc(int *input, char *file, int *prunepattern, bool is_last)
+{
+	char	*result;
+
+	if (!is_last)
+	{
+		heredoc_getline(file, &result);
+		if (heredoc_get_fd(result, input, file, prunepattern))
+		{
+			free (result);
+			return (1);
+		}
+		return (0);
+	}
+	else
+		return (error_syntax("newline", prunepattern));
+}
+
+int	get_io_type(char **file, unsigned short *listcont,
+			bool *islast, int *prunepattern)
+{
+	*islast = 0;
+	*file = downcast_wstr(listcont, 1);
+	if (is_symbol(listcont))
+	{
+		error_syntax(*file, prunepattern);
+		free (file);
+		return (1);
+	}
+	return (0);
+}
+
 int	get_redirs(t_list **args, int *input, int *output)
 {
 	size_t			i;
@@ -14,6 +93,7 @@ int	get_redirs(t_list **args, int *input, int *output)
 	char			*file;
 	int				*prunepattern;
 	t_list			*list;
+	bool			islast;
 
 	prunepattern = ft_calloc(ft_lstsize(*args), sizeof(*prunepattern));
 	i = 0;
@@ -22,9 +102,11 @@ int	get_redirs(t_list **args, int *input, int *output)
 	{
 		if (is_redir((unsigned short *)list->content))
 		{
+			islast = 1;
 			type = get_type((unsigned short *)list->content);
 			if (list->next && list->next->content)
 			{
+				islast = 0;
 				file = downcast_wstr(list->next->content, 1);
 				if (is_symbol(list->next->content))
 				{
@@ -52,8 +134,8 @@ int	get_redirs(t_list **args, int *input, int *output)
 				{
 					if (*output > STDERR_FILENO)
 						close(*output);
-					*output = open(file, O_CREAT|O_WRONLY|O_APPEND, S_IRUSR|
-								   S_IWUSR|S_IRGRP|S_IWGRP|S_IROTH);
+					*output = open(file, O_CREAT | O_WRONLY | O_APPEND, S_IRUSR
+							| S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 					if (*output == -1)
 						return (error_file(file, prunepattern));
 				}
@@ -66,8 +148,8 @@ int	get_redirs(t_list **args, int *input, int *output)
 				{
 					if (*output > STDERR_FILENO)
 						close(*output);
-					*output = open(file, O_CREAT|O_WRONLY|O_TRUNC, S_IRUSR|
-								   S_IWUSR|S_IRGRP|S_IWGRP|S_IROTH);
+					*output = open(file, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR
+							| S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 					if (*output == -1)
 						return (error_file(file, prunepattern));
 				}
